dec5bcc77791bfe36c6c18f95d23aea0eeaba972
Cristian Camilo Cardozo Moreno <cristian.cardozo@globant.com>
2022-03-09 16:57:46 -0500

    Cambio de la maduración de rest con http verbs.

--- src/main/java/com/camcar/customer/controller/CustomerController.java
+++ src/main/java/com/camcar/customer/controller/CustomerController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,15 +19,13 @@ import com.camcar.customer.model.Customer;
 import com.camcar.customer.service.CustomerService;
 
 @RestController
-@RequestMapping("/customer")
 public class CustomerController {
 	
 	@Autowired
 	private CustomerService customerService; 
 
-	@RequestMapping(value ="/test", method = RequestMethod.GET)
+	@GetMapping("/test")
     public String test() {
-		System.out.println("pase por get 2");
 		
 		Customer customer = new Customer();
 //		customer.setId(1);
@@ -33,29 +35,29 @@ public class CustomerController {
         return customer.getName();
     }
 	
-	@RequestMapping(value = "/new", method = RequestMethod.POST)
+	@PostMapping("/customer")
 	@ResponseStatus(HttpStatus.CREATED)
 	public boolean createCustomer(@RequestBody Customer customer) {
 		return customerService.createCustomer(customer);
 	}
 	
-	@RequestMapping(value = "/modify/{id}", method = RequestMethod.PUT)
+	@PutMapping("/customer/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public boolean updateCustomer(@PathVariable("id") int id, @RequestBody Customer customer) {
 		return customerService.updateCustomer(id, customer);
 	}
 	
-	@RequestMapping(value = "/obtainAll", method = RequestMethod.GET)
+	@GetMapping("/customer")
 	public List<Customer> getAllCustomers() {
 		return customerService.selectAllCustomers();
 	}
 	
-	@RequestMapping(value = "/obtainById/{id}", method = RequestMethod.GET)
+	@GetMapping("/customer/{id}")
 	public Customer getCustomerById(@PathVariable("id") int id) {
 		return customerService.selectCustomerById(id);
 	}
 	
-	@RequestMapping(value = "/clean/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping("customer/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public boolean deleteCustomer(@PathVariable("id") int id) {
 		return customerService.deleteCustomer(id);
