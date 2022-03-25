a504284a234adb1131221effcd01522ec5325bc9
Cristian Camilo Cardozo Moreno <cristian.cardozo@globant.com>
2022-03-25 12:11:22 -0500

    Se implementa gestión de excepciones

---
+++ src/main/java/com/camcar/customer/controller/events/ExceptionsHandler.java
@@ -0,0 +1,40 @@
+package com.camcar.customer.controller.events;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+@ControllerAdvice
+public class ExceptionsHandler {
+
+	@ExceptionHandler(value = {IllegalArgumentException.class})
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e){
+//		e.getStackTrace();
+		System.out.println("Exception ");
+//		if(e instanceof )
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+	}
+	
+	
+	@ExceptionHandler(value = {ResponseStatusException.class})
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<Object> handleResponseStatusException(ResponseStatusException e){
+//		e.getStackTrace();
+		System.out.println("Exception ");
+//		if(e instanceof )
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(value = {Exception.class})
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public ResponseEntity<Object> handleException(Exception e){
+//		e.getStackTrace();
+		System.out.println("Exception s");
+//		if(e instanceof )
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
